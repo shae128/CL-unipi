1f93438d49b28ae1bae34458f5ba9e849732e983
shae128 <slr772s@gmail.com>
2020-07-04 18:12:07 +0200

    Updated app1 to compare two files

--- app1.py
+++ app1.py
@@ -1,35 +1,22 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import sys
 import codecs
 import nltk
 
-def main(file1):
+# Calculate Sentences' length
+def sentencesLenCal(sentences):
 
-    # Declare function scope variables
+    ######################################
+    # Declare function's scope variables #
+    ######################################
 
     # Sentences length list
     sentenceLens = []
     # Sentences token length sum
     sumST = 0
 
-    # Tokens length list
-    tokensLens = []
-    # Tokens characters length sum
-    sumTL = 0
-
-    #open the first file
-    inputFile= codecs.open(file1, 'r', 'utf-8')
-    #read the first file
-    rawFile = inputFile.read()
-    #call english.pickle
-    sent_tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-    #tokenize in sentences
-    sentences = sent_tokenizer.tokenize(rawFile)
-    #tokenize in words
-    tokens = nltk.word_tokenize(rawFile)
-
-    # Create a list to store Sentences length
+    # Fill the list the Sentences length
     for sentence in sentences:
         #tokenize Sentences in words
         sentenceTokens = nltk.word_tokenize(sentence)
@@ -43,9 +30,22 @@ def main(file1):
     # Sentences average length
     sentenceAvgLen = sumST / len(sentenceLens)
 
+    return sentenceAvgLen
 
 
-    # Create a list to store tokens length
+# Calculate tokens' length
+def tokensLenCal(tokens):
+
+    ######################################
+    # Declare function's scope variables #
+    ######################################
+
+    # Tokens length list
+    tokensLens = []
+    # Tokens characters length sum
+    sumTL = 0
+
+    # Fill the list of tokens length
     for token in tokens:
         tokensLens.append(len(token))
  
@@ -54,9 +54,36 @@ def main(file1):
         sumTL += length
                
     # Tokens average length
-    tokensAvgLen = sumTL / len(tokensLens)
+    return sumTL / len(tokensLens)
 
+
+
+def main(file1,file2):
+
+    #open files
+    inputFile1= codecs.open(file1, 'r', 'utf-8')
+    inputFile2= codecs.open(file2, 'r', 'utf-8')
+    #read files
+    rawFile1 = inputFile1.read()
+    rawFile2 = inputFile2.read()
+
+    #call english.pickle
+    sent_tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+    #tokenize in sentences
+    sentences1 = sent_tokenizer.tokenize(rawFile1)
+    sentences2 = sent_tokenizer.tokenize(rawFile2)
+
+    #tokenize in words
+    tokens1 = nltk.word_tokenize(rawFile1)
+    tokens2 = nltk.word_tokenize(rawFile2)
+
+
+    sentenceAvgLen1 = sentencesLenCal(sentences1)
+    tokensAvgLen1 = tokensLenCal(tokens1)
 #
+    sentenceAvgLen2 = sentencesLenCal(sentences2)
+    tokensAvgLen2 = tokensLenCal(tokens2)
 #    for token in tokens:
 #        print(token.encode('utf-8'))
 #        print("------------------")
@@ -66,9 +93,17 @@ def main(file1):
 #    print(tokens)
 #    print ("sentences lenght: ", sentenceLens, len(sentenceLens))
 
-    print ("Sentences: ", len(sentences))
-    print ("Sentences average length: ", sentenceAvgLen)
-    print ("Tokens: ", len(tokens))
-    print ("Tokens average length: ", tokensAvgLen)
+    print ("The Adventures Of Sherlock Holmes: ")
+    print ("Sentences: ", len(sentences1))
+    print ("Sentences average length: ", sentenceAvgLen1)
+    print ("Tokens: ", len(tokens1))
+    print ("Tokens average length: ", tokensAvgLen1)
+    print ("#############################################")
+    print ("The Time Machine: ")
+    print ("Sentences: ", len(sentences2))
+    print ("Sentences average length: ", sentenceAvgLen2)
+    print ("Tokens: ", len(tokens2))
+    print ("Tokens average length: ", tokensAvgLen2)
 
-main(sys.argv[1])
+
+main(sys.argv[1], sys.argv[2])
