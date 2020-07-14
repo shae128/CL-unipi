21a9c403c04b94df08179865b6a8e55804cddba2
shae128 <slr772s@gmail.com>
2020-07-14 16:21:43 +0200

    Added a function to find top ten name entities with PERSON tag

--- app2.py
+++ app2.py
@@ -12,27 +12,59 @@ from prettytable import PrettyTable
 ###### Order Dictionary #######
 ###############################
 def orderDic(dic):
-    return sorted(dic.items(), key=lambda x: x[1], reverse=True)
+    return sorted(dic, key=lambda x: (dic[x]['count']), reverse=True)
 
 
-###############################
-###### Remove punctuations####3
-###############################
-def removePunc(tokens):
-    noPunc = []
-    for token in tokens:
-        if not token in [",",".",";",":","-","_","[","]","{","}","'","?","(",")",'"',"''","``","!","$","#"]:
-            noPunc.append(token)
+########################################
+########### Name Entity finder #########
+########################################
+def topTenNE(senteces):
 
-    return noPunc
+    # to hold each name entity and it's count and Sentences
+    NE_CS = {}
+
+    # PoS tagging tokes
+    for sentence in senteces:
+        tokens = nltk.word_tokenize(sentence)
+        PosTags = nltk.pos_tag(tokens)
+        ne_chunks = nltk.ne_chunk(PosTags)
+
+        for node in ne_chunks:
+            try:
+                node.label()
+            except AttributeError:
+                pass
+            else:
+                if node.label() == "PERSON":
+                    for leave in node.leaves():
+                        if leave[0] not in NE_CS.keys():
+                            NE_CS[leave[0]] = { 'count' : 1, 'senteces' : [] }
+                            NE_CS[leave[0]]['senteces'].append(sentence)
+                        else:
+                            NE_CS[leave[0]]['count'] += 1
+                            NE_CS[leave[0]]['senteces'].append(sentence)
+    
+
+    # sort the NE_CS dictionary
+    NE_CS_Sorted = orderDic(NE_CS)
+
+    # Create a list to hold top ten Name entities and their related info
+    topTen_NE = []
+    for i in range(0,10):
+        tempDic = {}
+        the_name = NE_CS_Sorted[i]
+        tempDic['Name'] = the_name
+        tempDic['count'] = NE_CS[the_name]['count']
+        tempDic['senteces'] = NE_CS[NE_CS_Sorted[i]]['senteces']
+        topTen_NE.append(tempDic)
 
 
-
+    return topTen_NE
 
 
 
 ######################
-# The Main Function  #
+# The Main Functi n  #
 ######################
 def main(file1,file2):
 
@@ -51,9 +83,11 @@ def main(file1,file2):
     sentences2 = sent_tokenizer.tokenize(rawFile2)
 
     #tokenize in words
-    tokens1 = nltk.word_tokenize(rawFile1)
-    tokens2 = nltk.word_tokenize(rawFile2)
-
-
+#    tokens1 = nltk.word_tokenize(rawFile1)
+#    tokens2 = nltk.word_tokenize(rawFile2)
+#
+    # Find Person names
+    topTenPerson1 = topTenNE(sentences1)
+    topTenPerson2 = topTenNE(sentences2)
 
 main(sys.argv[1], sys.argv[2])
